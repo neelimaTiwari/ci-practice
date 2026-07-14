b02adff71f955850e9c293c6129893c20dc742a3
neelimatiwari695-cyber <neelimatiwari695@gmail.com>
2026-07-14 11:24:26 +0530

    chang app.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static String greet() {
-        return "Hello from CI pipeline";
+        return "Hello from CI pipeline my first ci pipline";
     }
 
     public static void main(String[] args) {
